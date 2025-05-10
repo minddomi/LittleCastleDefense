17141422c48cbbd8f3e1151d196a28d29c25e24d
minddomi <kms06075@gmail.com>
2025-05-10 17:16:35 +0900

    button test

---
+++ Assets/Scripts/Button/SlideLeftPanel.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlideLeftPanel : MonoBehaviour
+{
+    public RectTransform panel;         // 사각형 패널 (오른쪽 고정)
+    public float targetWidth = 500f;    // 스르륵 펼쳐질 최종 너비
+    public float speed = 10f;
+
+    private bool isOpen = false;
+
+    void Start()
+    {
+        SetWidth(0f); // 처음엔 숨김
+    }
+
+    void Update()
+    {
+        float goal = isOpen ? targetWidth : 0f;
+        float current = panel.sizeDelta.x;
+        float next = Mathf.Lerp(current, goal, Time.deltaTime * speed);
+
+        SetWidth(next);
+    }
+
+    void SetWidth(float width)
+    {
+        panel.sizeDelta = new Vector2(width, panel.sizeDelta.y);
+    }
+
+    public void TogglePanel()
+    {
+        isOpen = !isOpen;
+    }
+}
